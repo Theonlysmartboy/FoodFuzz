27988a35f7dc9e48a4a41e93579c3c64c94abb44
TheOnlySmartBoy <o2jose43@gmail.com>
2019-06-19 18:43:05 +0300

    Completedc writting the login logic and adding permissions to manifest file

--- app/src/main/java/com/otemainc/foodfuzz/com/oteaminc/foodfuzz/db/BackgroundWorker.java
+++ app/src/main/java/com/otemainc/foodfuzz/com/oteaminc/foodfuzz/db/BackgroundWorker.java
@@ -1,14 +1,16 @@
 package com.otemainc.foodfuzz.com.oteaminc.foodfuzz.db;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
-import com.otemainc.foodfuzz.LoginActivity;
-
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -17,15 +19,16 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-public class BackgroundWorker extends AsyncTask<String,Void,Void> {
+public class BackgroundWorker extends AsyncTask<String, Void, String> {
     protected Context context;
+    AlertDialog alert;
     public BackgroundWorker(Context ctx) {
         context = ctx;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
-    protected Void doInBackground(String... strings) {
+    protected String doInBackground(String... strings) {
         String type = strings[0];
         String email = strings[1];
         String pass = strings[2];
@@ -45,6 +48,17 @@ public class BackgroundWorker extends AsyncTask<String,Void,Void> {
                 loginwriter.flush();
                 loginwriter.close();
                 loginout.close();
+                InputStream loginin = con.getInputStream();
+                BufferedReader loginreader = new BufferedReader(new InputStreamReader(loginin, StandardCharsets.ISO_8859_1));
+                String result = "";
+                String line;
+                while ((line = loginreader.readLine())!=null){
+                    result += line;
+                }
+                loginreader.close();
+                loginin.close();
+                con.disconnect();
+                return result;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -56,12 +70,14 @@ return null;
 
     @Override
     protected void onPreExecute() {
-        super.onPreExecute();
-    }
+        alert = new AlertDialog.Builder(context).create();
+        alert.setTitle("Login");
+            }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPostExecute(String result) {
+        alert.setMessage(result);
+        alert.show();
     }
 
     @Override
