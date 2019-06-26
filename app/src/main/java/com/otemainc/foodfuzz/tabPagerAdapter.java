98f1d86ae188f3a72760d5e2d9ab04066b540e14
TheOnlySmartBoy <o2jose43@gmail.com>
2019-06-26 19:14:21 +0300

    Added a tabbedview and begun adding the adapter

--- app/src/main/java/com/otemainc/foodfuzz/tabPagerAdapter.java
+++ app/src/main/java/com/otemainc/foodfuzz/tabPagerAdapter.java
@@ -1,13 +1,20 @@
 package com.otemainc.foodfuzz;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 public class tabPagerAdapter  extends FragmentStatePagerAdapter {
+    String[] tabArray = new String[]{"Food","Drinks",};
 
     public tabPagerAdapter(FragmentManager fm) {
         super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
     }
 
     @Override
