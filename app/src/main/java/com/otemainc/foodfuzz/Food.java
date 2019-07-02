852a455297905ef0ef230b832a1afa2ae29427e6
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-02 20:29:54 +0300

    Begun writing the logic for getting food

--- app/src/main/java/com/otemainc/foodfuzz/Food.java
+++ app/src/main/java/com/otemainc/foodfuzz/Food.java
@@ -6,14 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Food extends Fragment {
-
-
+    private static final String TAG = "Food";
+    ListView food;
     public Food() {
         // Required empty public constructor
     }
@@ -23,7 +24,9 @@ public class Food extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food, container, false);
+        View view = inflater.inflate(R.layout.fragment_food, container, false);
+        food = view.findViewById(R.id.FoodContainer);
+        return view;
     }
 
 }
