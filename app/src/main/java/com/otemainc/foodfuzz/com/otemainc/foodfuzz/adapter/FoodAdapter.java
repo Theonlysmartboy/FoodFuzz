5adf9043689c64bb9b9ab99af34f874e1efc8def
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-02 19:52:16 +0300

    Begun writing the getters and setters for the food adapter

--- app/src/main/java/com/otemainc/foodfuzz/com/otemainc/foodfuzz/adapter/FoodAdapter.java
+++ app/src/main/java/com/otemainc/foodfuzz/com/otemainc/foodfuzz/adapter/FoodAdapter.java
@@ -1,4 +1,39 @@
 package com.otemainc.foodfuzz.com.otemainc.foodfuzz.adapter;
 
+import android.media.Image;
+
 public class FoodAdapter {
+    private Image image;
+    private String title;
+    private String cost;
+
+    public FoodAdapter(Image image, String title, String cost) {
+        this.image = image;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
 }
